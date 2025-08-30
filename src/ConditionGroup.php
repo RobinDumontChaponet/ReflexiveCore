4abb2e482d0cb0505ff8f6727eaadf9a3aaff6b1
Robin Dumont-Chaponet <RobinDumontChaponet@users.noreply.github.com>
2025-08-30 22:29:49 +0200

    conditions…

--- src/ConditionGroup.php
+++ src/ConditionGroup.php
@@ -4,15 +4,15 @@ declare(strict_types=1);
 
 namespace Reflexive\Core;
 
-class ConditionGroup
+abstract class ConditionGroup
 {
 	protected array $conditions = [];
 	protected ?Operator $nextOperator = null;
 
-	protected array $parameters = [];
-	private string $columnName = 'cGroup';
+	// protected array $parameters = [];
+	protected(set) string $name = 'cGroup';
 
-	public function __construct(
+	protected function __construct(
 		?Condition $firstCondition = null
 	) {
 		if($firstCondition !== null)
@@ -25,7 +25,7 @@ class ConditionGroup
 		if(!empty($this->conditions) && empty($this->nextOperator))
 			throw new \TypeError('Condition added to query chain without operator before previous condition.');
 
-		$this->conditions[$condition->columnName.'_'.count($this->conditions)] = [
+		$this->conditions[$condition->name.'_'.count($this->conditions)] = [
 			'condition' => $condition,
 			'operator' => $this->nextOperator,
 		];
@@ -70,32 +70,5 @@ class ConditionGroup
 	public function hasConditions(): bool
 	{
 		return !empty($this->conditions);
-	}
-
-	public function bake(int &$index, bool $quoteNames = true, ?Operator $operator = null, int &$prettify = -1): array
-	{
-		$queryString = '';
-		$parameters = [];
-		if($prettify > -1 && count($this->conditions) > 1) {
-			$prettify++;
-		}
-
-		foreach($this->conditions as $conditionArray) {
-			$baked = $conditionArray['condition']->bake($index, $quoteNames, $conditionArray['operator'], $prettify);
-
-			$queryString.= ($prettify > 0 ? str_repeat("\t", $prettify) : '').$baked['queryString'];
-			$parameters += $baked['parameters'];
-
-			$index++;
-		}
-
-		if(count($this->conditions) > 1) {
-			$queryString = '('.($prettify > 0 ? PHP_EOL : ''). rtrim($queryString, PHP_EOL) .($prettify > 0 ? PHP_EOL.str_repeat("\t", --$prettify) : '').')';
-		}
-
-		return [
-			'queryString' => $operator?->value.$queryString,
-			'parameters' => $parameters,
-		];
 	}
 }
